a0e2fbd16f349aedee35fa106fa1c94f021166ed
Danny <danny.kettle@dkwebservices.co.uk>
2026-04-20 22:49:03 +0100

    Addded comments to public interface ICheckout

--- MyLibrary/ICheckout.cs
+++ MyLibrary/ICheckout.cs
@@ -1,9 +1,20 @@
 ﻿namespace MyLibrary;
 
-interface ICheckout
+/// <summary>
+/// Represents a checkout system that allows scanning items and calculating a total price
+/// based on pricing rules and special offers.
+/// </summary>
+public interface ICheckout
 {
+    /// <summary>
+    /// Scans an item into the checkout basket.
+    /// </summary>
+    /// <param name="item">The SKU of the item being scanned.</param>
     void Scan(string item);
 
+    /// <summary>
+    /// Calculates the total price of all scanned items, including any special pricing rules.
+    /// </summary>
+    /// <returns>The total price of the basket.</returns>
     int GetTotalPrice();
 }
-
